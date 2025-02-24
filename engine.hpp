553956b856fc40cee786d615efb87c275fa88d61
wangwiza <e1496974@u.nus.edu>
2025-02-24 14:30:45 +0800

    base data structures defined

--- engine.hpp
+++ engine.hpp
@@ -5,10 +5,82 @@
 #define ENGINE_HPP
 
 #include <chrono>
-#include "order.hpp"
+#include <mutex>
+#include <vector>
+#include <memory>
+#include <unordered_map>
 
 #include "io.hpp"
-#include "order_book.hpp"
+
+// Order Linked List
+struct Order
+{
+	uint32_t order_id;
+	uint32_t price;
+	uint32_t count;
+	std::string instrument;
+	uint32_t execution_id;
+	bool is_sell;
+
+	Order(uint32_t order_id, uint32_t price, uint32_t count, std::string instrument, bool is_sell) : order_id(order_id), price(price), count(count), instrument(instrument), is_sell(is_sell) {}
+};
+
+// Store the orders by price
+struct PriceNode
+{
+	uint32_t price;
+	std::vector<std::shared_ptr<Order>> orders;
+	PriceNode *next;
+	std::mutex m;
+
+	PriceNode(uint32_t price = 0) : price(price), next(nullptr) {}
+};
+
+struct BuyList
+{
+	PriceNode head;
+	void insert(std::shared_ptr<Order> order);
+	void match_order(std::shared_ptr<Order> order);
+
+	BuyList() : head() {}
+};
+
+struct SellList
+{
+	PriceNode head;
+	void insert(std::shared_ptr<Order> order);
+	void match_order(std::shared_ptr<Order> order);
+
+	SellList() : head() {}
+};
+
+class InstrumentBook
+{
+public:
+	BuyList buy_list;
+	SellList sell_list;
+
+	void process_sell_order(std::shared_ptr<Order> order);
+	void process_buy_order(std::shared_ptr<Order> order);
+	void process_cancel_order(std::shared_ptr<Order> order);
+};
+
+class OrderBook
+{
+public:
+	InstrumentBook &get_instrument_book(std::string instrument);
+	Order &get_order(uint32_t order_id);
+	void record_order(std::shared_ptr<Order> order);
+
+private:
+	std::unordered_map<std::string, InstrumentBook> instrument_map;
+	std::unordered_map<uint32_t, std::shared_ptr<Order>> order_map;
+	std::shared_mutex m;
+};
+
+// Order Book
+
+// Engine Stuff
 
 struct Engine
 {
@@ -16,11 +88,7 @@ public:
 	void accept(ClientConnection conn);
 
 private:
-	OrderBook order_book;
 	void connection_thread(ClientConnection conn);
-	void process_buy_order(Order order);
-	void process_sell_order(Order order);
-	void process_cancel_order(uint32_t order_id);
 };
 
 inline std::chrono::microseconds::rep getCurrentTimestamp() noexcept
