d3b3c6879b46bf0e85954d4859d0ca7bb4e8fe18
wangwiza <e1496974@u.nus.edu>
2025-02-24 16:43:25 +0800

    format

--- hashmap/concurrent_hash_map.hpp
+++ hashmap/concurrent_hash_map.hpp
@@ -1,27 +1,37 @@
+#pragma once
+
 #include <functional>
 #include <list>
 #include <vector>
+#include <mutex>
 
 const size_t DEFAULT_INITIAL_SIZE = 100;
 const float DEFAULT_LOAD_FACTOR = 0.7f;
 
-template <typename K, typename V> class HashMap {
+template <typename K, typename V>
+class HashMap
+{
 private:
   using Bucket = std::list<std::pair<K, V>>;
   std::vector<Bucket> buckets;
+  std::vector<std::shared_mutex> bucket_mutexes;
   size_t numElements;
   float maxLoadFactor = DEFAULT_LOAD_FACTOR;
 
-  size_t hash(const K &key) const {
+  size_t hash(const K &key) const
+  {
     return std::hash<K>{}(key) % buckets.size();
   }
 
-  void rehash() {
+  void rehash()
+  {
     size_t newSize = buckets.size() * 2;
     std::vector<Bucket> newBuckets(newSize);
 
-    for (auto &bucket : buckets) {
-      for (auto &pair : bucket) {
+    for (auto &bucket : buckets)
+    {
+      for (auto &pair : bucket)
+      {
         size_t newIndex = std::hash<K>{}(pair.first) % newSize;
         newBuckets[newIndex].push_back(pair);
       }
@@ -34,12 +44,15 @@ public:
   HashMap(size_t initialSize = DEFAULT_INITIAL_SIZE)
       : buckets(initialSize), numElements(0) {}
 
-  void insert(const K &key, const V &value) {
+  void insert(const K &key, const V &value)
+  {
     size_t index = hash(key);
 
     // Check for existing key
-    for (auto &pair : buckets[index]) {
-      if (pair.first == key) {
+    for (auto &pair : buckets[index])
+    {
+      if (pair.first == key)
+      {
         pair.second = value;
         return;
       }
@@ -50,15 +63,19 @@ public:
     numElements++;
 
     // Check load factor
-    if (static_cast<float>(numElements) / buckets.size() > maxLoadFactor) {
+    if (static_cast<float>(numElements) / buckets.size() > maxLoadFactor)
+    {
       rehash();
     }
   }
 
-  bool get(const K &key, V &value) const {
+  bool get(const K &key, V &value) const
+  {
     size_t index = hash(key);
-    for (const auto &pair : buckets[index]) {
-      if (pair.first == key) {
+    for (const auto &pair : buckets[index])
+    {
+      if (pair.first == key)
+      {
         value = pair.second;
         return true;
       }
@@ -66,12 +83,15 @@ public:
     return false;
   }
 
-  bool remove(const K &key) {
+  bool remove(const K &key)
+  {
     size_t index = hash(key);
     auto &bucket = buckets[index];
 
-    for (auto it = bucket.begin(); it != bucket.end(); ++it) {
-      if (it->first == key) {
+    for (auto it = bucket.begin(); it != bucket.end(); ++it)
+    {
+      if (it->first == key)
+      {
         bucket.erase(it);
         numElements--;
         return true;
