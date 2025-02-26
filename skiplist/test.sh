ef53a6f198bfcd5b0d72411bd392c61c0bc684da
David <david@dzhy.dev>
2025-02-26 23:19:27 +0800

    Don't attempt to compile at each iteration

--- skiplist/test.sh
+++ skiplist/test.sh
@@ -8,29 +8,29 @@ echo "Press Ctrl+C to stop manually"
 
 counter=1
 
+# Exit if compilation fails
+if ! clang++ -g -O3 -Wall -Wextra -pedantic -Werror -std=c++20 -pthread main.cpp -fsanitize=thread; then
+  exit_code=$?
+  echo "Compilation failed with exit code $exit_code"
+  echo "Stopping after $counter iterations"
+  exit $exit_code
+fi
+
+
+echo "Compilation successful, running executable..."
 while true; do
   echo "-------------------------------------------"
   echo "Iteration #$counter"
   echo "-------------------------------------------"
 
-  # Run the compile command and if successful, run the program with time
-  if clang++ -g -O3 -Wall -Wextra -pedantic -Werror -std=c++20 -pthread main.cpp; then
-    echo "Compilation successful, running executable..."
-    if time ./a.out; then
-      echo "Execution successful"
-    else
-      exit_code=$?
-      echo "Execution failed with exit code $exit_code"
-      echo "Stopping after $counter iterations"
-      break
-    fi
+  if time ./a.out; then
+    echo "Execution successful"
   else
     exit_code=$?
-    echo "Compilation failed with exit code $exit_code"
+    echo "Execution failed with exit code $exit_code"
     echo "Stopping after $counter iterations"
     break
   fi
-
   counter=$((counter + 1))
   echo ""
 done
